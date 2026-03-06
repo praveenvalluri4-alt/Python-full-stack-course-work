4ffbb90e975b5cb6c14852712c1f4e1d353b6b7a
praveenvalluri4-alt <praveenvalluri4@gmail.com>
2026-03-06 13:40:59 +0530

    Add files via upload

---
+++ day-2/data_types.py
@@ -0,0 +1,29 @@
+Student_ID = 'CGH2926'
+student_name ='praveen'
+Batch_No='PFS-HYD-050'
+Email_ID= 'praveenvalluri4@gmail.com'
+Age=21
+State='andhra pradesh'
+Location='hyderabad'
+Student_No='9014640455'
+Parent_No='9182104131'
+Github='https://github.com/praveenvalluri4-alt'
+College_Name='asn degree college'
+USN_Number=92
+Department='Bachelor of computer applications'
+Qualification='UG (Bachelor Degrees)'
+Graduation ='76%'
+Year_of_Passing= 2026
+Skills=('Python', 'Flask', 'HTML', 'CSS')
+Mode_of_Study='Offline'
+
+print('------------Personal Information----------------')
+print(f'Student ID: {Student_ID}')
+print(f'Batch No: {Batch_No}')
+print(f'Email ID: {Email_ID}')
+
+
+print('------------Academic Information----------------')
+print(f'Department: {Department}')
+print(f'College Name: {College_Name}')
+print(f'Qualification: {Qualification}')
