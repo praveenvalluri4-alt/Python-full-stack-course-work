916697d1a978099ff2eb0ad0a82c3fd3af71e94c
praveenvalluri4-alt <praveenvalluri4@gmail.com>
2026-03-11 17:18:54 +0530

    Add files via upload

---
+++ day-4/control.py
@@ -0,0 +1,35 @@
+"""
+products ={'bread':30, 'butter':50, 'milk':20, 'sugar':45, 'salt':60}
+for item in products:
+    print("product name:",item)
+    print("product price:",products[item])
+    print("buy now | add to cart")
+    print("add to fav")
+    print('-------------')
+"""    """
+n=int(input("enter the number:"))
+for i in range(1,21):
+    print(f"{n}*{i}={n*i}")
+"""
+#------------break and continue----------
+'''
+for i in range(10):
+    if i==5:
+        break
+    print(i)
+else:
+    ("the iteration is somplete")
+
+    '''
+
+#----------------login------------
+
+
+pin=12345
+for i range(5):
+    user_pin=int(input("enter your pin"))
+    if user_pin == pin:
+    print("login successful")
+
+
+
